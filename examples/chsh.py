6aab71795f1c2bd4173df762fe2ebcac55291021
Vincent Russo <vincentrusso1@gmail.com>
2015-01-12 18:20:57 -0500

    Started to add examples

---
+++ examples/chsh.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+#------------------------------------------------------------------------------
+# Name:        chsh.py
+# Purpose:     This example  
+#
+# Author:      Vincent Russo (vrusso@cs.uwaterloo.ca)
+#
+# References: [1] Navascues, M. and Pironio, S. and A. Acin. A convergent 
+#                 hierarchy of semidefinite programs characterizing the set of 
+#                 quantum correlations. New Journal of Physics, 2008, 073013. 
+#
+# Created:     1/11/2015
+# Copyright:   (c) Vincent Russo 2015
+# Licence:     GNU
+#------------------------------------------------------------------------------
+'''
